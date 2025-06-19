af3f21808697923864ff603eac2fee83f5e66f7b
TeddyMedjio <nganmedjiojeanclaude@gmail.com>
2025-06-19 11:46:35 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist_Mono, Open_Sans } from "next/font/google";
+import { Open_Sans } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./components/navbar";
